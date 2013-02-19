93dd6759812599a83d9b607eebaa9d905cc19add
Roberto Aloi <prof3ta@gmail.com>
2013-02-19 12:53:19 +0100

    More readable (and meaningful) diffs. Use SHA to spot differences in non-printable content.

--- src/keynote-to-text.erl
+++ src/keynote-to-text.erl
@@ -3,15 +3,18 @@
 -export([main/1]).
 
 -define(INDEX, "index.apxl").
+-define(SHA_KEY, "keynote").
+
+-record(state, {acc = [], slide_id, slide_content = []}).
 
 -spec main([string()]) -> no_return().
 main([Filename]) ->
     Files = files(Filename),
-    Strings = strings(Filename),
-    io:format("FILES~n", []),
-    [io:format("~p~n", [File]) || File <- Files],
-    io:format("STRINGS~n", []),
-    [io:format("~p~n", [String]) || String <- Strings],
+    Slides = slides(Filename),
+    print(title("Embedded Files")),
+    [print(File) || File <- Files],
+    print(title("Slides")),
+    [print(Slide) || Slide <- Slides],
     erlang:halt(0).
 
 -spec files(string()) -> [string()].
@@ -20,16 +23,46 @@ files(Filename) ->
     {ok, Files} = zip:zip_list_dir(Zip),
     filter([Name || {zip_file, Name, _Info, _, _, _} <- lists:sort(Files)]).
 
--spec strings(string()) -> [string()].
-strings(Filename) ->
+-spec slides(string()) -> [string()].
+slides(Filename) ->
     {ok, [{?INDEX, Xml}]} = zip:extract(Filename, [{file_list, [?INDEX]}, memory]),
-    F = fun({characters, C}, Acc) ->
-                [C|Acc];
-           (_,Acc) ->
-                Acc
+    F = fun({characters, C},
+            #state{acc = Acc} = State) ->
+                State#state{acc = [C|Acc]};
+           ({startElement, _Uri, "slide", _Prefix, Attributes},
+            #state{acc = Acc} = State) ->
+                SlideId = slide_id(get_attribute("ID", Attributes)),
+                SlideTitle = title("Slide " ++ SlideId),
+                State#state{acc           = [SlideTitle|Acc],
+                            slide_id      = SlideId,
+                            slide_content = []};
+           ({endElement, _Uri, "slide", _Prefix},
+            #state{acc = Acc, slide_id = SlideId} = State) ->
+                SlideSha = sha(lists:sort(State#state.slide_content)),
+                State#state{acc = ["SHA (Slide " ++ SlideId ++ "): " ++ SlideSha|Acc]};
+           ({startElement, _Uri, "master-slide", _Prefix, Attributes},
+            #state{acc = Acc} = State) ->
+                SlideId = slide_id(get_attribute("ID", Attributes)),
+                SlideName = get_attribute("name", Attributes),
+                SlideTitle = title("Master Slide " ++ SlideId ++ " (" ++ SlideName ++ ")"),
+                State#state{acc           = [SlideTitle|Acc],
+                            slide_id      = SlideId,
+                            slide_content = []};
+           ({endElement, _Uri, "master-slide", _Prefix},
+            #state{acc = Acc, slide_id = SlideId} = State) ->
+                SlideSha = sha(lists:sort(State#state.slide_content)),
+                State#state{acc = ["SHA (Slide " ++ SlideId ++ "): " ++ SlideSha|Acc]};
+           ({startElement, _Uri, "notes", _Prefix, _Attributes},
+            #state{acc = Acc} = State) ->
+                State#state{acc = [subtitle("Notes")|Acc]};
+           ({startElement, Uri, Tag, Prefix, _Attributes},
+            #state{slide_content = SlideContent} = State) ->
+                State#state{slide_content = [{Uri, Tag, Prefix}|SlideContent]};
+           (_Other, State) ->
+                State
         end,
-    {ok, Strings, _} = erlsom:parse_sax(Xml, [], F),
-    Strings.
+    {ok, State, _} = erlsom:parse_sax(Xml, #state{}, F),
+    lists:reverse(State#state.acc).
 
 filter(Filenames) ->
     filter(Filenames, []).
@@ -44,3 +77,26 @@ filter([".iWTrash/" ++ _|T], Acc) ->
     filter(T, Acc);
 filter([H|T], Acc) ->
     filter(T, [H|Acc]).
+
+get_attribute(Tag, Attributes) ->
+    {value, Attribute} = lists:keysearch(Tag, 2, Attributes),
+    {attribute, Tag, _, _, Value} = Attribute,
+    Value.
+
+title(String) ->
+    "==> " ++ String.
+
+subtitle(String) ->
+    "---" ++ String.
+
+sha(Term) ->
+    <<Mac:160/integer>> = crypto:sha_mac(?SHA_KEY, term_to_binary(Term)),
+    lists:flatten(io_lib:format("~40.16.0b", [Mac])).
+
+slide_id("BGSlide-" ++ X) ->
+    integer_to_list(list_to_integer(X) + 1);
+slide_id("BGMasterSlide-" ++ X) ->
+    "Master " ++ integer_to_list(list_to_integer(X) + 1).
+
+print(String) ->
+    io:format("~p~n", [String]).
